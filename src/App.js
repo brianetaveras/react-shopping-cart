d9773d0fc64960873fc0f3cd755ac282fc30d105
brianetaveras <taveras574@gmail.com>
2019-12-19 19:26:39 -0500

    Converted the whole thing to context yooo

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import React, { useState} from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
-import ProductContext from './contexts/ProductContext';
+// Context API
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-		setCart([item, ...cart])
-	};
+  const addItem = item => {
+    // add the given item to the cart
+    setCart([item, ...cart]);
+  };
 
-	return (
+  return (
+    <div className="App">
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{cart}}>
+          <Navigation cart={cart} />
 
-		<div className="App">
-			<ProductContext.Provider value={{products, addItem}}>
-			<Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route exact path="/cart" component={ShoppingCart} />
 
-			{/* Routes */}
-			<Route exact path="/" component={Products} />
-
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-			</ProductContext.Provider>
-		</div>
-	);
+        </CartContext.Provider>
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
