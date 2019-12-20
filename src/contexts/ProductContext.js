b605bcedada17bf5e25ec892ea14ec179a27f104
brianetaveras <taveras574@gmail.com>
2019-12-19 19:00:58 -0500

    modified product context

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,8 +1,8 @@
-import React, {useContext} from 'react'
+import React, {createContext} from 'react';
 
-export default function ProductContext(){
 
-    
+const ProductContext = createContext();
 
-}
+
+export default ProductContext;
 
